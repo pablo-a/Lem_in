69557bf1a43374ad064e4b080cbfdc8fef3b4571
pablo-a <pabloabril@hotmail.fr>
2016-05-12 12:58:00 +0200

    oklm ca avance

--- src/parsing.c
+++ src/parsing.c
@@ -1,40 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lemin.c                                            :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabril <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/05/11 12:16:23 by pabril            #+#    #+#             */
-/*   Updated: 2016/05/12 12:54:27 by pabril           ###   ########.fr       */
+/*   Created: 2016/05/12 12:28:03 by pabril            #+#    #+#             */
+/*   Updated: 2016/05/12 12:57:31 by pabril           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lemin.h"
-#include "ft_printf.h"
-#include "get_next_line.h"
-
-int		init_env(t_env *env)
+int		parse(t_env *env)
 {
-	t_room	*tab_room[SIZE_TAB];
-	t_ant	*ant;
-
-	env->nb_ants = 0;
-	env->tab_room = tab_room;
-	env->ants = ant;
-	return (0);
-}
-
-int		main(void)
-{
-	t_env	env;
-	char	*str;
-
-	init_env(&env);
-	parse(&env);
-	while (get_next_line(0, &str) > 0)
-	{
-		printf("%s\n", str);
-	}
 	return (0);
 }
