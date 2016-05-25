b3d300be2bf63d1a887d7e3378e27b9bfb7a4952
pablo-a <pabloabril@hotmail.fr>
2016-05-25 15:11:50 +0200

    ajout des maps, bonne route sur la trap.map mais mauvaise dans la house

--- src/resolve.c
+++ src/resolve.c
@@ -6,7 +6,7 @@
 /*   By: pabril <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/19 13:03:04 by pabril            #+#    #+#             */
-/*   Updated: 2016/05/25 12:15:47 by pabril           ###   ########.fr       */
+/*   Updated: 2016/05/25 15:03:46 by pabril           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,18 @@ int		possible_to_resolve(t_env *env, t_room *current_pos)
 		while (current_voisin != NULL)
 		{
 			if (current_voisin->room->visite == 0)
+			{
 				if (possible_to_resolve(env, current_voisin->room) == 1)
 				{
 					current_pos->visite = 0;
 					return (1);
 				}
+			}
 			current_voisin = current_voisin->next;
 		}
 	}
 	current_pos->visite = 0;
 	return (0);
-}
-
-int		get_marked_path(t_env *env, t_room *current_pos)
-{
-	t_path	*path;
-
-	path = new_path();
-	while (current_pos->type != ENTRY)
-	{
-		append_node_path(path, current_pos);
-		current_pos = PERE(current_pos);
-	}
-	append_node_path(path, current_pos);
-	append_path(env->pathes, path);
-	return (1);
 }
 
 int		show_pathes(t_env *env)
@@ -77,31 +64,51 @@ int		show_pathes(t_env *env)
 	return (1);
 }
 
+int		get_marked_path(t_env *env, t_room *current_pos)
+{
+	t_path	*path;
+
+	path = new_path();
+	while (current_pos->type != ENTRY && PERE(current_pos) != NULL)
+	{
+		append_node_path(path, current_pos);
+		current_pos = PERE(current_pos);
+	}
+	append_node_path(path, current_pos);
+	append_path(env->pathes, path);
+	return (1);
+}
+
 int		mark_path(t_env *env, int num, t_room *current_pos)
 {
 	t_node	*current_voisin;
 	t_room	*next_room;
 
 	next_room = NULL;
+	ft_printf("CURRENT POSITION = %s\n", current_pos->name);
 	VISITE(current_pos) = num;
 	if (LINK != NULL)
 	{
 		current_voisin = LINK->first;
 		while (current_voisin != NULL)
 		{
-			if (VISITE(current_voisin->room) < num && (POIDS(current_pos) + 1 <
-			POIDS(current_voisin->room) || POIDS(current_voisin->room) == -1))
+			ft_printf("position = %s\n", current_pos->name);
+			ft_printf("voisin = %s\n", current_voisin->room->name);
+			if (VISITE(current_voisin->room) < num && ((POIDS(current_pos) + 1 <
+			POIDS(current_voisin->room)) || POIDS(current_voisin->room) == -1))
 			{
 				POIDS(current_voisin->room) = POIDS(current_pos) + 1;
 				PERE(current_voisin->room) = current_pos;
 			}
-			if ((next_room == NULL || (POIDS(current_voisin->room) <
-				POIDS(next_room))) && VISITE(current_voisin->room) < num)
+			if (current_voisin->room->type == ENTRY)
+				;
+			else if ((VISITE(current_voisin->room) < num) ||
+					POIDS(current_pos) + 1 < POIDS(PERE(current_voisin->room)) + 1)
 				next_room = current_voisin->room;
+			if (next_room != NULL && VISITE(current_voisin->room) < num)
+				mark_path(env, num, next_room);
 			current_voisin = current_voisin->next;
 		}
-		if (next_room != NULL)
-			mark_path(env, num, next_room);
 	}
 	return (1);
 }
@@ -120,15 +127,18 @@ int		resolve(t_env *env)
 	while (!same_pathes(env))
 	{
 		mark_path(env, numero_path, env->starting_room);
+		ft_printf("ok");
 		get_marked_path(env, env->ending_room);
+		ft_printf("ok2");
 		numero_path++;
 		reset_poids(env);
+		ft_printf("ok3\n");
 	}
 	env->pathes->nb_path--;
 	env->pathes->last = env->pathes->last->prev_path;
 	env->pathes->last->next_path = NULL;
 	//free last path;
-	show_tab(*env);
+	//show_tab(*env);
 	show_pathes(env);
 	return (1);
 }
